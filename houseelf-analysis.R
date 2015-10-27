9716d11ef02f89b0a539f1f186c89b8815b92d89
Joshua Epstein <jmepstein12@ufl.edu>
2015-10-27 12:33:06 -0400

    Code to import the elf data file.

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -1,3 +1,5 @@
-# This analysis is intended to Print the average GC-contents for large and 
+# This analysis is intended to print the average GC-contents for large and 
 # small eared elves
 
+#Importing elf data:
+elf<-read.csv("houseelf_earlength_dna_data.csv", header=T)
