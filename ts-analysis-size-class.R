862c76481d11776248faded94e0ea5b0f92e1655
Joshua Epstein <jmepstein12@ufl.edu>
2015-10-27 12:20:33 -0400

    Add function for determining size class from weight

--- ts-analysis-size-class.R
+++ ts-analysis-size-class.R
@@ -2,3 +2,13 @@ get_data<- function(){
   data<- read.csv("mammal_surveys.csv")
   return(data)
 }
+
+get_size_class<- function(weight){
+  if (weight>50){
+    size_class<- "large"
+  }
+  else{
+    size_class<- "small"
+  }
+  return(size_class)
+}
