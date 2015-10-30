2e979bf38bf009d2bdc5bf41c749abebbd926af1
Joshua Epstein <jmepstein12@ufl.edu>
2015-10-30 12:35:13 -0400

    Adding function to determine GC Content

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -3,3 +3,12 @@
 
 #Importing elf data:
 elf<-read.csv("houseelf_earlength_dna_data_1.csv", header=T)
+
+GC_Content<- function(dnaseq){
+#Function to calculate the GC content of a sequence
+sequence<-str_to_upper(dnaseq)
+Gs <- str_count(sequence, 'G')
+Cs <- str_count(sequence, 'C')
+gc_content <- (Gs + Cs) / str_length(sequence) * 100 
+return(gc_content)
+}
