fca497ff882e6931822fda6c573113cd315b3d52
Joshua Epstein <jmepstein12@ufl.edu>
2015-10-30 13:47:19 -0400

    Addition of code to determine each row's ID, earlength class, and GC content in a loop, and input values in a data frame. Then save the data frame as a csv file.

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -1,6 +1,8 @@
 # This analysis is intended to print the average GC-contents for large and 
 # small eared elves
 
+library(stringr)
+
 #Importing elf data:
 elf<-read.csv("houseelf_earlength_dna_data_1.csv", header=T)
 
@@ -18,3 +20,21 @@ get_size_class <- function(seq){
    ear_lengths <- ifelse(seq > 10, "large", "small")
    return(ear_lengths)
 }
+
+#Write some new code that creates a data frame with information about the 
+#individual ID, the earth length class, and the gc-content for each individual
+
+#Empty data frame:
+output <- data.frame()
+
+size_and_GC_content<- for(row in 1:nrow(elf)){
+  #For loop to get the earlength category and GC content for each row and store them
+  ear_size_per_row <- get_size_class(elf[row,]$earlength)
+  GCContent <- GC_Content(elf[row,]$dnaseq)
+  elf_output<- data.frame(elf[row,]$id, ear_size_per_row, GCContent)
+  output<-rbind(output, elf_output)
+}
+print(output)
+
+#Export table to csv file:
+write.csv(output, file = "git_elf_earlength_GCcontent.csv")
